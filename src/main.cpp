23e9e82b1e19d7a9fd7bc921b003cd61767aa0d5
Tomasz Witczak <TomaszMWitczak@gmail.com>
2019-10-07 20:11:13 +0200

    Move shader compilation code to another function

--- src/main.cpp
+++ src/main.cpp
@@ -1,83 +1,103 @@
 // //////////////////////////////////////////////////////////// Includes //
 #include "opengl-headers.h"
 
+#include <fstream>
 #include <iostream>
+#include <string>
 
-static void glfw_error_callback(int error, const char *description) {
-    fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+// ///////////////////////////////////////////////////////////////////// //
+static
+void glfwErrorCallback(int const errorNumber,
+                       char const *description) {
+    std::cerr << "GLFW;"
+              << "Error " << errorNumber << "; "
+              << "Description: " << description;
+
 }
 
+int shaderProgram;
 
-void renderTriangle() {
-    const unsigned int SCR_WIDTH = 800;
-    const unsigned int SCR_HEIGHT = 600;
+void buildAndCompileShaders() {
+    char const *vertexShaderSource =
+            "#version 330 core\n"
+            "layout (location = 0) in vec3 aPos;\n"
+            "void main()\n"
+            "{\n"
+            "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+            "}\n\0";
 
-    const char *vertexShaderSource = "#version 330 core\n"
-                                     "layout (location = 0) in vec3 aPos;\n"
-                                     "void main()\n"
-                                     "{\n"
-                                     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-                                     "}\0";
-    const char *fragmentShaderSource = "#version 330 core\n"
-                                       "out vec4 FragColor;\n"
-                                       "void main()\n"
-                                       "{\n"
-                                       "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-                                       "}\n\0";
+    char const *fragmentShaderSource =
+            "#version 330 core\n"
+            "out vec4 FragColor;\n"
+            "void main()\n"
+            "{\n"
+            "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+            "}\n\0";
 
-    // build and compile our shader program
-    // ------------------------------------
-    // vertex shader
+    // Vertex shader
     int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
     glCompileShader(vertexShader);
-    // check for shader compile errors
+
+    // Check for shader compile errors
     int success;
     char infoLog[512];
+
     glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
     if (!success) {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
         std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
-                  << infoLog << std::endl;
+                  << infoLog
+                  << std::endl;
     }
+
     // fragment shader
     int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
     glCompileShader(fragmentShader);
+
     // check for shader compile errors
     glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
     if (!success) {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
         std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
                   << infoLog << std::endl;
     }
     // link shaders
-    int shaderProgram = glCreateProgram();
+    shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
     // check for linking errors
     glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
     if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog
-                  << std::endl;
+        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
+                  << infoLog << std::endl;
     }
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
+}
 
-    // set up vertex data (and buffer(s)) and configure vertex attributes
-    // ------------------------------------------------------------------
+void renderTriangle() {
+    const unsigned int SCR_WIDTH = 800;
+    const unsigned int SCR_HEIGHT = 600;
+
+    buildAndCompileShaders();
+
     float vertices[] = {
             -0.5f, -0.5f, 0.0f, // left
             0.5f, -0.5f, 0.0f, // right
-            0.0f, 0.5f, 0.0f  // top
+            0.0f, 0.5f, 0.0f,  // top
     };
 
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and
+    // then configure vertex attributes(s).
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -85,19 +105,22 @@ void renderTriangle() {
                  GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
-                          (void *) 0);
+                          (void *) nullptr);
     glEnableVertexAttribArray(0);
 
-    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO
+    // as the vertex attribute's bound vertex buffer object so afterwards we can
+    // safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally
+    // modify this VAO, but this rarely happens. Modifying other VAOs requires a
+    // call to glBindVertexArray anyways so we generally don't unbind VAOs (nor
+    // VBOs) when it's not directly necessary.
     glBindVertexArray(0);
 
-
     // uncomment this call to draw in wireframe polygons.
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
@@ -105,14 +128,16 @@ void renderTriangle() {
     // draw our first triangle
     glUseProgram(shaderProgram);
     glBindVertexArray(
-            VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+            VAO); // seeing as we only have a single VAO there's no need to bind it
+    // every time, but we'll do so to keep things a bit more organized
     glDrawArrays(GL_TRIANGLES, 0, 3);
     // glBindVertexArray(0); // no need to unbind it every time
 
-    // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+    // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved
+    // etc.)
     // -------------------------------------------------------------------------------
-//            glfwSwapBuffers(window);
-//            glfwPollEvents();
+    //            glfwSwapBuffers(window);
+    //            glfwPollEvents();
 
     // optional: de-allocate all resources once they've outlived their purpose:
     // ------------------------------------------------------------------------
@@ -123,7 +148,7 @@ void renderTriangle() {
 // //////////////////////////////////////////////////////////////// Main //
 int main(int, char **) {
     // ------------------------------------------------- Setup window -- //
-    glfwSetErrorCallback(glfw_error_callback);
+    glfwSetErrorCallback(glfwErrorCallback);
     if (!glfwInit())
         return 1;
 
@@ -134,7 +159,7 @@ int main(int, char **) {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // 3.2+ only
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);           // Required on Mac
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Required on Mac
 #else
     // GL 4.3 + GLSL 430
     const char *glsl_version = "#version 430";
@@ -143,13 +168,13 @@ int main(int, char **) {
     glfwWindowHint(GLFW_OPENGL_PROFILE,
                    GLFW_OPENGL_CORE_PROFILE); // 3.2+ only
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT,
-                   GL_TRUE);           // 3.0+ only
+                   GL_TRUE); // 3.0+ only
 #endif
 
     // Create window with graphics context
-    GLFWwindow *window = glfwCreateWindow(1280, 720,
-                                          "Tomasz Witczak 216920 - zadanie 1",
-                                          nullptr, nullptr);
+    GLFWwindow *window = glfwCreateWindow(
+            1280, 720, "Tomasz Witczak 216920 - zadanie 1", nullptr,
+            nullptr);
     if (window == nullptr) {
         return 1;
     }
@@ -175,30 +200,40 @@ int main(int, char **) {
     ImGui::CreateContext();
     ImGuiIO &io = ImGui::GetIO();
     (void) io;
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;   // Enable Gamepad Controls
+    // io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard
+    // Controls io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;   // Enable
+    // Gamepad Controls
 
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(glsl_version);
 
     // Setup style
     ImGui::StyleColorsDark();
-    //ImGui::StyleColorsClassic();
+    // ImGui::StyleColorsClassic();
 
     // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+    // - If no fonts are loaded, dear imgui will use the default font. You can
+    // also load multiple fonts and use ImGui::PushFont()/PopFont() to select
+    // them.
+    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you
+    // need to select the font among multiple.
+    // - If the file cannot be loaded, the function will return nullptr. Please
+    // handle those errors in your application (e.g. use an assertion, or display
+    // an error and quit).
+    // - The fonts will be rasterized at a given size (w/ oversampling) and stored
+    // into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which
+    // ImGui_ImplXXXX_NewFrame below will call.
     // - Read 'misc/fonts/README.txt' for more instructions and details.
-    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-    //io.Fonts->AddFontDefault();
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
-    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-    //IM_ASSERT(font != NULL);
+    // - Remember that in C/C++ if you want to include a backslash \ in a string
+    // literal you need to write a double backslash \\ !
+    // io.Fonts->AddFontDefault();
+    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
+    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
+    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
+    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
+    // ImFont* font =
+    // io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f,
+    // nullptr, io.Fonts->GetGlyphRangesJapanese()); IM_ASSERT(font != nullptr);
 
     bool show_demo_window = true;
     bool show_another_window = false;
@@ -207,10 +242,14 @@ int main(int, char **) {
     // Main loop
     while (!glfwWindowShouldClose(window)) {
         // Poll and handle events (inputs, window resize, etc.)
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
-        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
-        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to
+        // tell if dear imgui wants to use your inputs.
+        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to
+        // your main application.
+        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input
+        // data to your main application. Generally you may always pass all inputs
+        // to dear imgui, and hide them from your application based on those two
+        // flags.
         glfwPollEvents();
 
         // Start the Dear ImGui frame
@@ -218,31 +257,39 @@ int main(int, char **) {
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-//        if (show_demo_window)
-//            ImGui::ShowDemoWindow(&show_demo_window);
+        // 1. Show the big demo window (Most of the sample code is in
+        // ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear
+        // ImGui!).
+        //        if (show_demo_window)
+        //            ImGui::ShowDemoWindow(&show_demo_window);
 
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair
+        // to created a named window.
         {
             static float f = 0.0f;
             static int counter = 0;
 
             ImGui::Begin(
-                    "Hello, world!"); // Create a window called "Hello, world!" and append into it.
+                    "Hello, world!"); // Create a window called "Hello, world!"
+            // and append into it.
 
             ImGui::Text(
-                    "This is some useful text.");          // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window",
-                            &show_demo_window); // Edit bools storing our window open/close state
+                    "This is some useful text."); // Display some text (you can
+            // use a format strings too)
+            ImGui::Checkbox(
+                    "Demo Window",
+                    &show_demo_window); // Edit bools storing our window open/close state
             ImGui::Checkbox("Another Window", &show_another_window);
 
             ImGui::SliderFloat("float", &f, 0.0f,
-                               1.0f);             // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color",
-                              (float *) &clear_color); // Edit 3 floats representing a color
+                               1.0f); // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3(
+                    "clear color",
+                    (float *) &clear_color); // Edit 3 floats representing a color
 
             if (ImGui::Button(
-                    "Button")) // Buttons return true when clicked (most widgets return true when edited/activated)
+                    "Button")) // Buttons return true when clicked (most
+                // widgets return true when edited/activated)
                 counter++;
             ImGui::SameLine();
             ImGui::Text("counter = %d", counter);
@@ -253,15 +300,17 @@ int main(int, char **) {
             ImGui::End();
         }
 
-//        // 3. Show another simple window.
-//        if (show_another_window) {
-//            ImGui::Begin("Another Window",
-//                         &show_another_window); // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-//            ImGui::Text("Hello from another window!");
-//            if (ImGui::Button("Close Me"))
-//                show_another_window = false;
-//            ImGui::End();
-//        }
+        //        // 3. Show another simple window.
+        //        if (show_another_window) {
+        //            ImGui::Begin("Another Window",
+        //                         &show_another_window); // Pass a pointer to our
+        //                         bool variable (the window will have a closing
+        //                         button that will clear the bool when clicked)
+        //            ImGui::Text("Hello from another window!");
+        //            if (ImGui::Button("Close Me"))
+        //                show_another_window = false;
+        //            ImGui::End();
+        //        }
 
         // Rendering
         ImGui::Render();
--- src/opengl-headers.h
+++ src/opengl-headers.h
@@ -6,18 +6,24 @@
 #include "imgui_impl_opengl3.h"
 
 #if defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
+
 #include <GL/gl3w.h>
+
 #elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
+
 #include <GL/glew.h>
+
 #elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
 
 #include <glad/glad.h>
 
 #else
+
 #include IMGUI_IMPL_OPENGL_LOADER_CUSTOM
+
 #endif
 
 #include <GLFW/glfw3.h>
 
 // ///////////////////////////////////////////////////////////////////// //
-#endif //FIRST_PARAGRAPH_OPENGL_HEADERS_H
+#endif // FIRST_PARAGRAPH_OPENGL_HEADERS_H
