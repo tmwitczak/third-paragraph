78b5aea5d49cb15172ce2ae4ccdef3ed39430ea1
Tomasz Witczak <TomaszMWitczak@gmail.com>
2019-11-08 23:13:19 +0100

    Prepare repository for task 3

--- readme.md
+++ readme.md
@@ -1,9 +1,10 @@
-# Programowanie Aplikacji Graficznych - Zadanie 2
+# Programowanie Aplikacji Graficznych - Zadanie 3
 
 1. **Środowisko implementacji**
     * CMake *3.8*
-    * Microsoft Visual Studio Enterprise 2017 *15.6.7*
+    * Microsoft Visual Studio Enterprise 2019 *16.3.8*
 2. **Użyte biblioteki**
+    * [assimp](https://github.com/assimp/assimp.git) *4.1.0*
     * [glad](http://glad.dav1d.de/#profile=compatibility&language=c&specification=gl&loader=on&api=gl%3D4.6) *0.1.27*
     * [glfw](https://github.com/glfw/glfw.git) *3.4.0*
     * [glm](https://github.com/g-truc/glm.git) *0.9.9*
--- thirdparty/thirdparty.cmake
+++ thirdparty/thirdparty.cmake
@@ -2,6 +2,29 @@
 find_package(OpenGL REQUIRED)
 set(OPENGL_LIBRARY ${OPENGL_LIBRARIES})
 
+# assimp
+find_library(ASSIMP_LIBRARY "assimp" "/usr/lib" "/usr/local/lib")
+find_path(ASSIMP_INCLUDE_DIR "assimp/mesh.h" "/usr/include" "/usr/local/include")
+
+if((NOT ASSIMP_LIBRARY) OR (NOT ASSIMP_INCLUDE_DIR))
+	set(ASSIMP_DIR "${THIRDPARTY_DIR}/assimp")
+	
+	message("Unable to find assimp, cloning...")
+    execute_process(COMMAND git submodule update --init ${ASSIMP_DIR}
+                    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+
+	set(BUILD_SHARED_LIBS OFF CACHE INTERNAL "Build package with shared libraries.")
+	set(ASSIMP_BUILD_ASSIMP_TOOLS OFF CACHE INTERNAL "If the supplementary tools for Assimp are built in addition to the library.")
+	set(ASSIMP_BUILD_TESTS OFF CACHE INTERNAL "If the test suite for Assimp is built in addition to the library.")
+
+    add_subdirectory("${ASSIMP_DIR}")
+
+	set(ASSIMP_LIBRARY "assimp")
+	set(ASSIMP_INCLUDE_DIR "${ASSIMP_DIR}/include" "${CMAKE_CURRENT_BINARY_DIR}/thirdparty/assimp/include")
+endif()
+
+set(CMAKE_DEBUG_POSTFIX "")
+
 # glad
 set(GLAD_DIR "${THIRDPARTY_DIR}/glad")
 add_library("glad" STATIC "${GLAD_DIR}/src/glad.c")
